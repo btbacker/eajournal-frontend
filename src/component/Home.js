6f414a4a13a75c2fdb9b8949572eed82714454b7
btbacker <33066436+btbacker@users.noreply.github.com>
2020-12-10 12:22:18 -0800

    home

---
+++ src/component/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Button from 'react-bootstrap/Button';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+               <Button variant="success">Log In</Button>
+            </div>
+        );
+    }
+}
+
+export default Home;
