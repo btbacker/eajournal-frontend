7991e820f22e4b8fd9c8be6da8595f9d2b322a1e
btbacker <33066436+btbacker@users.noreply.github.com>
2020-12-10 12:52:45 -0800

    login

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 class Home extends Component {
     render() {
         return (
             <div>
-               <Button variant="success">Log In</Button>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="" />
+                    <Card.Body>
+                        <Button variant="success">Log In</Button>
+                        <Button variant="success">Sign Up</Button>
+                    </Card.Body>
+                </Card>
             </div>
         );
     }
---
+++ src/component/Login.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+class Login extends Component {
+    state = {
+        email: '',
+        password: '',
+    };
+    
+    handleChange = (e) => {
+        const {name, value} =  e.target
+        this.setState({ [name]: value })
+    };
+
+    handleSubmit = e => {
+        e.preventDefault()
+        this.props.handleSubmit(this.state)
+    }
+
+    handleErrors = () => {
+        return (
+            <div>
+                <ul>
+                    {this.state.errors.map(error => {
+                        return <li key={error}>{error}</li>
+                    })}
+                </ul>
+            </div>
+        )
+    }
+
+    render() {
+        const {email, password} = this.state
+        return (
+            <div className="login-form">
+                <Card style={{ width: '18rem' }}>
+                    <h1>Log In</h1>
+
+                    <form onSubmit={this.handleSubmit}>
+                        <input 
+                            name="email" 
+                            value={email} 
+                            type="text" 
+                            placeholder="Email" 
+                            onChange={this.handleChange} 
+                        /><br/>
+                        <input 
+                            name="password" 
+                            value={password} 
+                            type="password" 
+                            placeholder="Password" 
+                            onChange={this.handleChange} 
+                        /><br/>
+                        <Button className="submit-btn" type="submit" variant="success">Log In</Button>
+                    </form>
+
+                <div>
+                    {this.state.errors ? this.handleErrors() : null}
+                </div>
+                </Card>
+            </div>
+        )
+    }
+};
+
+export default Login;
